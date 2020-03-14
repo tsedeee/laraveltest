ad82043fb63ced390e1a4c3d6e47e2d71f37ff35
tsedeee <tseegii9512@gmail.com>
2020-03-15 00:39:46 +0800

    album to add image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -109,4 +109,23 @@ class ImageController extends Controller
         \Storage::delete(['public/'.$filePath]);
         return redirect('/album')->with('Message', 'Зураг амжилттай устлаа.');
     }
+    public function image(Request $request)
+    {
+        //dd($request->all());
+        $this->validate($request, [
+            'image' => 'required'
+        ]);
+
+        if($request->hasFile('image')){
+            $albumId = $request->id;
+            foreach($request->file('image') as $image){
+                $path = $image->store('uploads', 'public');
+                Image::create([
+                    'name' => $path,
+                    'album_id' => $albumId
+                ]);
+            }
+            return redirect()->back()->with("Message", "Зургийг амжилттай үүсгэлээ.");
+        }
+    }
 }
--- resources/views/images/gallery.blade.php
+++ resources/views/images/gallery.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
 
-<div class="container">
+    @include('images.addImageAlbum')
+
+    @if(Session::has('Message'))
+        <div class="alert alert-success">{{ Session::get('Message') }}</div>
+    @endif
     <h1>{{ $albums->name }}({{ $albums->images->count() }} - зураг байна.)</h1>
     <div class="row">
         @foreach ($albums->images as $album)
@@ -17,7 +21,7 @@
                     </form> --}}
 
                     <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
     Устгах
   </button>
 
@@ -51,3 +55,49 @@
     </div>
 </div>
 @endsection
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script>
+    $(document).ready(function(e){
+        $(".btn-add-more").click(function(){
+            //alert("Ok");
+            var html = $(".copy").html();
+            $(".initial-add-more").after(html);
+        })
+
+        $("body").on("click", ".remove", function(){
+            $(this).parents(".control-group").remove();
+        })
+    })
+</script>
+
+{{--<script type="text/javascript">
+
+    $(document).ready(function(){
+        $("#form").on('submit', (function(e){
+            e.preventDefault();
+            $.ajax({
+                url: "/album/create",
+                type: "POST",
+                data: new FormData(this),
+                contentType: false,
+                cache: false,
+                processData: false,
+                success: function(response){
+                    $('.show').html(response);
+                    $("#form")[0].reset();
+                    $("#errMsg").empty();
+                },
+                error: function(data){
+                    //console.log(data.responseJSON);
+                    var error = data.responseJSON;
+                    $("#errMsg").empty();
+                    $.each(error.errors, function(key, value){
+                        $("#errMsg").append('<p class="text-center text-danger">' + value + '</p>')
+                    });
+                }
+            })
+        }))
+    })
+</script> --}}
